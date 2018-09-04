8b6e42e611e096886212691a03337f5343b24b5c
Julien Palard <julien@palard.fr>
2018-09-04 10:43:40 +0200

    Use :memory: during tests to avoid duplicate key errors.

--- tests/test_views.py
+++ tests/test_views.py
@@ -10,84 +10,78 @@ from pasee.pasee import identification_app
 from tests import mocks
 
 
-@pytest.fixture(scope="module")
-def db():
-    connection = sqlite3.connect(":memory:")
-    cursor = connection.cursor()
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS users(
-            name TEXT PRIMARY KEY
-        );
-        """
-    )
-    cursor.execute("CREATE TABLE IF NOT EXISTS groups(name TEXT PRIMARY KEY);")
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS user_in_group(
-            id INTEGER PRIMARY KEY,
-            user TEXT,
-            group_name TEXT
-        );
-        """
-    )
-
-    cursor.execute(
-        """
-        INSERT INTO users(
-            name
-        ) VALUES (
-            "kisee-toto"
-        ), (
-            "kisee-restrictedguy"
-        ), (
-            "kisee-guytoadd"
+async def load_fake_data(app):
+    conn = app.authorization_backend.connection
+    with conn:
+        conn.execute(
+            """
+            CREATE TABLE IF NOT EXISTS users(
+                name TEXT PRIMARY KEY
+            );
+            """
         )
-        """
-    )
-    cursor.execute("INSERT INTO groups(name) VALUES ('staff')")
-    cursor.execute(
-        """
-        INSERT INTO user_in_group(
-            user, group_name
-        ) VALUES (
-            "kisee-toto", "staff"
-        )"""
-    )
-
-    cursor.execute(
-        """
-        INSERT INTO groups (
-            name
-        ) VALUES (
-            'get_group'
-        ), (
-            'get_group.staff'
+        conn.execute("CREATE TABLE IF NOT EXISTS groups(name TEXT PRIMARY KEY);")
+        conn.execute(
+            """
+            CREATE TABLE IF NOT EXISTS user_in_group(
+                id INTEGER PRIMARY KEY,
+                user TEXT,
+                group_name TEXT
+            );
+            """
         )
-        """
-    )
-    cursor.execute(
-        """
-        INSERT INTO user_in_group (
-            user, group_name
-        ) VALUES (
-            'kisee-toto', 'get_group.staff'
-        ), (
-            'kisee-toto', 'get_group'
-        )
-        """
-    )
 
-    connection.commit()
-    cursor.close()
-    connection.close()
-    yield
-    os.remove("tmp_test.db")
+        conn.execute(
+            """
+            INSERT INTO users(
+                name
+            ) VALUES (
+                "kisee-toto"
+            ), (
+                "kisee-restrictedguy"
+            ), (
+                "kisee-guytoadd"
+            )
+            """
+        )
+        conn.execute("INSERT INTO groups(name) VALUES ('staff')")
+        conn.execute(
+            """
+            INSERT INTO user_in_group(
+                user, group_name
+            ) VALUES (
+                "kisee-toto", "staff"
+            )"""
+        )
+
+        conn.execute(
+            """
+            INSERT INTO groups (
+                name
+            ) VALUES (
+                'get_group'
+            ), (
+                'get_group.staff'
+            )
+            """
+        )
+        conn.execute(
+            """
+            INSERT INTO user_in_group (
+                user, group_name
+            ) VALUES (
+                'kisee-toto', 'get_group.staff'
+            ), (
+                'kisee-toto', 'get_group'
+            )
+            """
+        )
 
 
 @pytest.fixture
-def client(loop, aiohttp_client, db):
+def client(loop, aiohttp_client):
     app = identification_app(settings_file="tests/test-settings.toml")
+    app.on_startup.append(load_fake_data)
     return loop.run_until_complete(aiohttp_client(app))
 
 
