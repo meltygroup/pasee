3d368e2c573e7e79a77ad1a610d84fe9227c9f20
Julien Palard <julien@palard.fr>
2018-09-04 10:25:27 +0200

    black on setup.py

--- setup.py
+++ setup.py
@@ -8,55 +8,52 @@ from setuptools import setup, find_packages
 
 def setup_package():
     setup(
-        name='pasee',
-        version='0.0.1',
-        description='HTTP server managing users.',
-        long_description='',
-        author='Julien Palard',
-        author_email='julien@palard.fr',
+        name="pasee",
+        version="0.0.1",
+        description="HTTP server managing users.",
+        long_description="",
+        author="Julien Palard",
+        author_email="julien@palard.fr",
         license="mit",
-        url='https://github.com/meltygroup/pasee',
+        url="https://github.com/meltygroup/pasee",
         classifiers=[
-            'Development Status :: 2 - Pre-Alpha',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: MIT License',
-            'Natural Language :: English',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.4',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6'
+            "Development Status :: 2 - Pre-Alpha",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Natural Language :: English",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
         ],
-        package_dir={'': 'src'},
+        package_dir={"": "src"},
         install_requires=[
-            'aiohttp>=3.2.1,<4',
-            'coreapi>=2.3.3,<3',
-            'cryptography>=2.2.2,<3',
-            'pyjwt>=1.6.3,<2',
-            'pytoml',
-            'shortuuid>=0.5.0',
+            "aiohttp>=3.2.1,<4",
+            "coreapi>=2.3.3,<3",
+            "cryptography>=2.2.2,<3",
+            "pyjwt>=1.6.3,<2",
+            "pytoml",
+            "shortuuid>=0.5.0",
         ],
-        entry_points={
-            'console_scripts': [
-                'pasee = pasee.__main__:main',
-            ]
-        },
+        entry_points={"console_scripts": ["pasee = pasee.__main__:main"]},
         extras_require={
-            'dev': [
-                'flake8',
-                'pylint==2.0.0.dev',  # Don't work on py37 without v2
-                'astroid==2.0.0.dev',  # Don't work on py37 without v2
-                'asynctest',
+            "dev": [
+                "flake8",
+                "pylint==2.0.0.dev",  # Don't work on py37 without v2
+                "astroid==2.0.0.dev",  # Don't work on py37 without v2
+                "asynctest",
                 "pytest-aiohttp==0.3.0",
-                'pytest',
-                'pytest-cov',
-                'aioresponses',
-                'detox',
-                'bandit',
-                'black',
-                'mypy',
+                "pytest",
+                "pytest-cov",
+                "aioresponses",
+                "detox",
+                "bandit",
+                "black",
+                "mypy",
             ]
         },
-        packages=find_packages('src'))
+        packages=find_packages("src"),
+    )
 
 
 if __name__ == "__main__":
