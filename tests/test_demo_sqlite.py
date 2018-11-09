f28ae89a36617d2904c3df52e3ebbc24287d3cf7
Lucas <lucas@Lucass-MBP.eeple.fr>
2018-11-09 17:23:26 +0100

    add tests for sqlite RunTimeError

--- tests/test_demo_sqlite.py
+++ tests/test_demo_sqlite.py
@@ -10,6 +10,46 @@ def sqlite_storage():
     return TestSqliteStorage({"file": ":memory:"})
 
 
-def test_get_authorizations_for_user__not_run_in_context_manager(sqlite_storage):
+def test_get_authorizations_for_user(sqlite_storage):
     with pytest.raises(RuntimeError):
         run(sqlite_storage.get_authorizations_for_user(""))
+
+
+def test_create_group(sqlite_storage):
+    with pytest.raises(RuntimeError):
+        run(sqlite_storage.create_group(""))
+
+
+def test_get_groups(sqlite_storage):
+    with pytest.raises(RuntimeError):
+        run(sqlite_storage.get_groups())
+
+
+def test_delete_group(sqlite_storage):
+    with pytest.raises(RuntimeError):
+        run(sqlite_storage.delete_group(""))
+
+
+def test_get_members_of_group(sqlite_storage):
+    with pytest.raises(RuntimeError):
+        run(sqlite_storage.get_members_of_group(""))
+
+
+def test_group_exists(sqlite_storage):
+    with pytest.raises(RuntimeError):
+        run(sqlite_storage.group_exists(""))
+
+
+def test_create_user(sqlite_storage):
+    with pytest.raises(RuntimeError):
+        run(sqlite_storage.create_user(""))
+
+
+def test_user_exists(sqlite_storage):
+    with pytest.raises(RuntimeError):
+        run(sqlite_storage.user_exists(""))
+
+
+def test_is_user_in_group(sqlite_storage):
+    with pytest.raises(RuntimeError):
+        run(sqlite_storage.is_user_in_group("", ""))
